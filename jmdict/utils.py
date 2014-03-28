0c0ee1f347f04c6b8c71b7955841f18500c0db70
Baptiste Lagarde <baptiste.lagarde@centraliens.net>
2014-03-28 17:32:32 +0100

    ADD: Function to download and validate an (optionally gzipped) XML against a DTD

---
+++ jmdict/utils.py
@@ -0,0 +1,23 @@
+import urllib2
+import gzip
+from StringIO import StringIO
+from lxml import etree
+
+
+isurl = lambda u: any([u.startswith(i + '://') for i in 'http https ftp'.split()])
+
+
+def get_flike(xml_path):
+    flike = StringIO(urllib2.urlopen(xml_path).read()) if isurl(xml_path) else open(xml_path)
+    return gzip.GzipFile(fileobj=flike) if xml_path.endswith('.gz') else flike
+
+
+def fetch_xml(xml_path, dtd=None):
+    flike = get_flike(xml_path)
+    xml = etree.XML(flike.read())
+    if dtd is not None:
+        print "Validating %s XML with %s DTD..." % (xml_path, dtd)
+        dtd = etree.DTD(get_flike(dtd))
+        dtd.validate(xml)
+    flike.seek(0)
+    return flike
